30e0f43a8aa7ed0f10f0a757f8106c3dd82deaba
MariosTech <contact@marios.tech>
2017-01-31 17:51:36 -0800

    Initial Commit

---
+++ Class Activity 20170131/Class Activity 20170131/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Class Activity 20170131
+//
+//  Created by Mario Personal on 1/31/17.
+//  Copyright © 2017 Mario's Tech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
