065a9a94975f0cd9ab454c40395ce43c42555604
MariosTech <contact@marios.tech>
2017-02-02 18:37:20 -0800

    Spacing

--- ClassActivity 20170202/ClassActivity 20170202/main.cpp
+++ ClassActivity 20170202/ClassActivity 20170202/main.cpp
@@ -14,21 +14,25 @@ const int numberOfScores = 4;
 
 template <typename T1> T1 getMax(T1 scores[]) {
     T1 max = 0;
+    
     for (int i = 0; i < numberOfScores; i++) {
         if (max < scores[i]) {
             max = scores[i];
         }
     }
+    
     return max;
 }
 
 int main(int argc, const char * argv[]) {
     int scores[numberOfScores];
+    
     cout << "Input " << numberOfScores << " scroes" << endl;
     for (int i = 0; i < numberOfScores; i++) {
         cin >> scores[i];
     }
     
     cout << "Max = " << getMax<int>(scores) << endl;
+    
     return 0;
 }
