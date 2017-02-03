581712780ad278171ba8f89a3f4b052f9ef92a11
MariosTech <contact@marios.tech>
2017-02-02 18:22:40 -0800

    Get Max amount of Scores

---
+++ ClassActivity 20170202/ClassActivity 20170202/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  ClassActivity 20170202
+//
+//  Created by Mario Personal on 2/2/17.
+//  Copyright © 2017 Mario's Tech. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+const int numberOfScores = 4;
+
+int getMax(int[numberOfScores]);
+
+int main(int argc, const char * argv[]) {
+    int scores[numberOfScores];
+    cout << "Input " << numberOfScores << " scroes" << endl;
+    for (int i = 0; i < numberOfScores; i++) {
+        cin >> scores[i];
+    }
+    
+    cout << "Max = " << getMax(scores) << endl;
+    return 0;
+}
+
+int getMax(int scores[numberOfScores]) {
+    int max = 0;
+    for (int i = 0; i < numberOfScores; i++) {
+        if (max < scores[i]) {
+            max = scores[i];
+        }
+    }
+    return max;
+}
